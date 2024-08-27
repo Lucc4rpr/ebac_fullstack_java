32bcc59ba0178cea8dbcc78f70dcafb830444c3e
Lucca <lucca07102002@gmail.com>
2024-08-27 00:23:18 -0300

    Finalizado exercicio de funcao com tipagem com typescript

---
+++ src/teste.ts
@@ -0,0 +1,17 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+
+const Multiplicacao = multiplicar(7, 4); 
+const saudacao = saudar("Fulano"); 
+
+
+
+
+console.log(Multiplicacao); 
+console.log(saudacao); 
