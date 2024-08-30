5376f4efdf0e4bffb62b0f8a79c47ccd92c1f33c
Lucca <lucca07102002@gmail.com>
2024-08-30 01:09:01 -0300

    fix: resolvendo pendencias e melhorando teste

--- cypress/e2e/agenda-contatos/home.cy.js
+++ cypress/e2e/agenda-contatos/home.cy.js
@@ -16,18 +16,23 @@ describe('Testes para a home', () => {
         cy.get('.sc-gLDzan input[type="email"]').type('exemplo@exemplo.com')
         cy.get('.sc-gLDzan input[type="tel"]').type('49999999999')
         cy.get('.adicionar').click()
+
+        cy.get('.sc-eDDNvR li').contains('Nome exemplo').should('exist')
     })
     
     it('Deve editar os dados do contato', () => {
         cy.get('.sc-eDDNvR li').filter(':contains("Nome exemplo")').parent().parent().parent().find('.edit').click()
-        cy.get('input[type="text"]').wait(3000).clear().wait(2000).type('exemploEDITADO')
-        cy.get('input[type="email"]').wait(3000).clear().wait(2000).type('exemplo@EDITADO.com')
-        cy.get('input[type="tel"]').wait(3000).clear().wait(2000).type('111111111111')
-        cy.get('.alterar').click().wait(5000)            
+        cy.get('input[type="text"]').clear().type('exemploEDITADO')
+        cy.get('input[type="email"]').clear().type('exemplo@EDITADO.com')
+        cy.get('input[type="tel"]').clear().type('111111111111')
+        cy.get('.alterar').click()         
+
+        cy.get('.sc-eDDNvR li').contains('exemploEDITADO').should('exist')
     })
     it('Deve apagar os itens', () => {
-        cy.get('.delete').click({ multiple: true }).wait(2000)
-    })
+        cy.get('.delete').click({ multiple: true })
 
+        cy.get('.sc-eDDNvR li').should('not.exist')
+    })
 
 })
